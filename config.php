4b8732da7046296ded993868e91cb0b7083dc784
Steve George <edu@pagerange.com>
2024-11-24 13:40:53 -0600

    Updated config to return array of settings

--- config.php
+++ config.php
@@ -5,15 +5,6 @@
 
 // Edit these options to meet your needs.
 
-// define('MYSQL_DBNAME', 'bikemike'); // database name
-// define('MYSQL_DBUSER', 'root'); // database username
-// define('MYSQL_DBPASS', ''); // database password
-// define('SQLITE_DBFILE', './database.sqlite'); // path to sqlite database file
-
-// // use only one of the following lines... comment out the other
-// // define('MYSQL_TABLES', ['t1', 't2', 't3']); // tables to migrate, or...
-// define('MYSQL_TABLES', []); // empty array migrates ALL tables
-
 return array (
     'MYSQL_DBNAME' => 'bikemike', // database name
     'MYSQL_DBUSER' => 'root', // database username
